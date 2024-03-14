746861697d43636e68a9098b00747b57b5e4519d
mohamedsalama77 <medoesv@gmail.com>
2024-03-15 00:21:07 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const inputBox = document.getElementById("input-box");
+
+const listContainer = document.getElementById("list-container")
+
+
+
+
+function addTask(){
+  if(inputBox.value === ''){
+    as.alert({
+      title: "Alert",
+      text: "You Must Adding Task",
+      icon: true,
+      image: false,
+    });
+  }
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData()
+}
+
+
+listContainer.addEventListener("click",function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    saveData()
+  }else if (e.target.tagName === "SPAN"){
+    as.confirm({
+      title: "Confirm",
+      text: 'Are You Sure You Want Delete This Task?',
+      icon: true,
+      image: "",
+      okBtnHtml: "Yes",
+      cancelBtnHtml: "No",
+      onConfirm: function(){
+        e.target.parentElement.remove()
+        saveData()
+      },
+      onCancel: function(){
+      }
+    });
+    saveData()
+  }
+},false)
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data")
+}
+
+window.onload = showTask()
---
+++ popup.js-main/README.md
@@ -0,0 +1,4 @@
+# AsPopup.js
+AsPopup.js (https://asbros.github.io/popup.js/) is a javascript library for creating beautiful, customizable popups, alerts, confirms, prompts, toasts.
+
+Visit https://asbros.github.io/popup.js/ for demo & documentation.
